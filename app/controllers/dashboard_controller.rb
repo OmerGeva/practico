b10770c109490922b904f5c9c16debb45e6b77d6
Alisa Silina <alisasilina@gmail.com>
2019-12-01 17:41:07 +0200

    dashboard cards base

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,8 @@
 class DashboardController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
-
   def home
-    @challenges = Challenge.all
+    @users_challenges = UsersChallenge.where(user: current_user)
     skip_authorization
   end
 end

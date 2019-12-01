3125a57e326858a613288a540d9f94ca8f396c47
Dean Wheeler <dfwheeler394@gmail.com>
2019-12-01 17:15:09 +0200

    added pudit:authorization to challenges controller for new and create methods

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -2,10 +2,12 @@ class ChallengesController < ApplicationController
   skip_before_action :authenticate_user!
   def new
     @challenge = Challenge.new
+    authorize @challenge
   end
 
   def create
     @challenge = Challenge.new
+    authorize @challenge
   end
 
   def show

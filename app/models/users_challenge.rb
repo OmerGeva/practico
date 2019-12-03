f6bcfbe20169d12344f18d72bcd4f46d6d751f3d
Alisa Silina <alisasilina@gmail.com>
2019-12-03 12:41:11 +0200

    events created for days in a row

--- app/models/users_challenge.rb
+++ app/models/users_challenge.rb
@@ -3,5 +3,19 @@ class UsersChallenge < ApplicationRecord
   belongs_to :challenge
 
   has_many :check_ins, dependent: :destroy
-  has_many :events
+  has_many :events, dependent: :destroy
+
+  after_create :create_events
+
+  private
+
+  def create_events
+    if challenge.count_type == 'in a row' && challenge.time_type == 'days'
+      date = challenge.starting_date
+      challenge.milestone.times do
+        Event.create(start_time: date, users_challenge: self, user: user)
+        date += 1.days
+      end
+    end
+  end
 end

d19e7c1c1c9cfe29bd8e5ba3b4d9001340c8b40e
Omer Geva <ogeva97@gmail.com>
2019-12-04 16:21:37 +0200

    sweeeeeeeeeeeeet alert

---
+++ app/javascript/plugins/init_sweetalert.js
@@ -0,0 +1,25 @@
+import swal from 'sweetalert';
+
+const initSweetAlertDeleteFriend = () => {
+
+  // const swalButton = document.querySelector(selector);
+  const linkbtns = document.querySelectorAll('#remove-friend-btn')
+  linkbtns.forEach((swalButton) => {
+    swalButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      swal("Are you sure?", {
+        dangerMode: true,
+        buttons: true,
+      })
+      .then((value) => {
+        if (value) {
+          document.getElementById(`remove-friend-${swalButton.dataset.friendid}`).click();
+          }
+        });
+    });
+
+  });
+
+};
+
+export { initSweetAlertDeleteFriend };

bdfce479209e01ee5af5a2a4108ca828fdffbd28
Omer Geva <ogeva97@gmail.com>
2019-12-05 11:03:24 +0200

    removed migration file

--- db/migrate/20191205083146_remove_scheduled_from_challenges.rb
+++
@@ -1,5 +0,0 @@
-class RemoveScheduledFromChallenges < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :challenges, :scheduled
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_083146) do
+ActiveRecord::Schema.define(version: 2019_12_04_213126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

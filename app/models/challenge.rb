d3d212924e01a86d6b807b1abd15f5452c9688a6
Omer Geva <ogeva97@gmail.com>
2019-12-08 10:02:54 +0200

    check ins on challenge page

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,6 +3,7 @@ class Challenge < ApplicationRecord
   has_many :chat_room
   has_many :users_challenges, dependent: :destroy
   has_many :users, through: :users_challenges
+  has_many :check_ins, through: :users_challenges
 
   TIME_TYPE = ['hours', 'days', 'weeks']
   COUNT_TYPE = ['in a row', 'total']

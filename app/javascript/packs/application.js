862b37fa18eb3b7d9ef165bed33bce03163380bc
Alisa Silina <alisasilina@gmail.com>
2019-12-08 13:11:32 +0200

    weekdays picker + slider range (needs to be fixed)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
 import "bootstrap";
-import "../plugins/flatpickr"
-import "../components/sidebar_toggle"
-import { previewImageOnFileSelect} from "../components/photo_preview"
-import { dataPickerHomepage} from "../components/datapicker"
+import "../plugins/flatpickr";
+import "../components/sidebar_toggle";
+import { previewImageOnFileSelect} from "../components/photo_preview";
+import { dataPickerHomepage} from "../components/datapicker";
 import { initSweetAlertDeleteFriend } from '../plugins/init_sweetalert';
-
+// import { getTrackStyle } from '../components/range_slider';
 
 
 initSweetAlertDeleteFriend();
@@ -14,3 +14,7 @@ if (document.URL.match('/challenges/new')) {
 }
 
 previewImageOnFileSelect();
+
+// if (/schedule/.test(window.location.href)) {
+//   getTrackStyle();
+// }

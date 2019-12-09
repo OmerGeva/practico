5b9de315f34e356c4c69c798e7ae4e41c9d38e64
Omer Geva <ogeva97@gmail.com>
2019-12-09 16:21:24 +0200

    sign  up asks for username first name and last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   before_action :authenticate_user!
   include Pundit
@@ -13,6 +14,13 @@ class ApplicationController < ActionController::Base
   end
 
   private
+   def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_create :set_picture
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -13,4 +15,8 @@ class User < ApplicationRecord
   has_many :check_ins, through: :users_challenges
 
   mount_uploader :photo, PhotoUploader
+
+  def set_picture
+    self.remote_photo_url = 'https://i.kym-cdn.com/photos/images/newsfeed/001/452/271/0f9'
+  end
 end

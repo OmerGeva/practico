b4994d4d09ff57399973ffa3e2f7d7fbcb3fae6a
Dean Wheeler <dfwheeler394@gmail.com>
2019-12-09 14:35:12 +0200

    correct phone number

--- app/services/send_twilio_message.rb
+++ app/services/send_twilio_message.rb
@@ -3,11 +3,11 @@ class SendTwilioMessage
     @alert = alert
     @account_sid = ENV['TWILIO_ACCOUNT_SID']
     @auth_token = ENV['TWILIO_TOKEN']
-    @service_number = "+14193181678" # From Twilio
+    @service_number = "+114696434598" # From Twilio
     @client = Twilio::REST::Client.new(@account_sid, @auth_token)
   end
 
-  def send_message(friend)
+  def send_message
     @client.messages.create(
       from: @service_number,
       to: to,

87c14d93cccd295325675c9ecc24f7b7f5653354
Omer Geva <ogeva97@gmail.com>
2019-12-10 12:20:32 +0200

    scheduling, accept friend, message board, trophies

--- app/javascript/components/chat.js
+++ app/javascript/components/chat.js
@@ -1,5 +1,5 @@
 
-if (document.URL.match(/challenges\/\d+/)) {
+if (document.URL.match(/challenges\/\d+$/)) {
 const chatHeader = document.querySelector('.chat-header');
 const chatBoard = document.querySelector('.chat-board');
 chatHeader.addEventListener(('click'), (event) => {
